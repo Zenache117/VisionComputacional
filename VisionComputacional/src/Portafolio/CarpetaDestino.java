719a482421baae71ae050158590451c2e8ff9158
Mauricio <mauricio.tamezelperro@gmail.com>
2023-04-02 12:11:10 -0600

    Clases para selección de archivos y carpetas

--- VisionComputacional/src/Portafolio/CarpetaDestino.java
+++ VisionComputacional/src/Portafolio/CarpetaDestino.java
@@ -1,5 +1,27 @@
 package Portafolio;
 
+import java.awt.FileDialog;
+import java.awt.Frame;
+
 public class CarpetaDestino {
 
+	public String selectFile() {
+		String rutaImagen = "";
+		// Leer CSV
+		// Crear un nuevo objeto FileDialog
+		FileDialog fileChooser = new FileDialog((Frame) null, "Seleccionar archivo CSV", FileDialog.LOAD);
+
+		// Mostrar el diálogo para que el usuario seleccione un archivo
+		fileChooser.setVisible(true);
+
+		// Verificar si el usuario seleccionó un archivo
+		if (fileChooser.getFile() != null) {
+
+			// Obtener el archivo seleccionado
+			rutaImagen = fileChooser.getDirectory() + fileChooser.getFile();
+
+		}
+		fileChooser.dispose();
+		return rutaImagen;
+	}
 }
--- VisionComputacional/src/Portafolio/SeleccionarArchivo.java
+++ VisionComputacional/src/Portafolio/SeleccionarArchivo.java
@@ -1,5 +1,19 @@
 package Portafolio;
 
+import java.awt.FileDialog;
+import java.awt.Frame;
+
 public class SeleccionarArchivo {
 
+	public String selectCarpet() {
+		// Seleccionar la carpeta destino para guardar la imagen transformada
+		String rutaCarpetaDestino;
+		// Crear un nuevo objeto FileDialog
+		FileDialog carpetaDestino = new FileDialog((Frame) null, "Seleccionar carpeta destino", FileDialog.LOAD);
+		carpetaDestino.setMode(FileDialog.SAVE);
+		carpetaDestino.setVisible(true);
+		rutaCarpetaDestino = carpetaDestino.getDirectory();
+		carpetaDestino.dispose();
+		return rutaCarpetaDestino;
+	}
 }
