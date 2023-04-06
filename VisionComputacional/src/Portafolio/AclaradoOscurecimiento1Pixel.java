2e369e0ccf0856eaac67ac053610fe77228a8a55
Mauricio <mauricio.tamezelperro@gmail.com>
2023-04-06 10:35:05 -0600

    Ajuste de modificación de un solo pixel

--- VisionComputacional/src/Portafolio/AclaradoOscurecimiento1Pixel.java
+++ VisionComputacional/src/Portafolio/AclaradoOscurecimiento1Pixel.java
@@ -30,25 +30,39 @@ public class AclaradoOscurecimiento1Pixel {
 		Imgproc.cvtColor(image, imageGray, Imgproc.COLOR_BGR2GRAY);
 		///////////////////////////////////////////////
 		// Obtener el tamaño de la imagen
-        int height = imageGray.rows();
-        int width = imageGray.cols();
+		int height = imageGray.rows();
+		int width = imageGray.cols();
 
-        // Preguntar al usuario las coordenadas del pixel a modificar
-        int row = -1, col = -1;
-        while (row < 0 || row >= height || col < 0 || col >= width) {
-            String input = JOptionPane.showInputDialog("Ingrese la fila y columna del pixel a modificar (x,y)");
-            String[] coords = input.split(",");
-            row = Integer.parseInt(coords[0].trim());
-            col = Integer.parseInt(coords[1].trim());
-        }
 
-        // Obtener el valor del pixel seleccionado
-        double[] pixelValue = imageGray.get(row, col);
-        ///////////////////////////////////////////////////////////////////////////////
-        
+		///////////////////////////////////////////////////////////////////////////////
+
 		// Crear matrices de imagenes en funcion a las dimenciones originales
 		Mat imagenDisminucionBrillo = new Mat(imageGray.rows(), imageGray.cols(), imageGray.type());
 		Mat imagenAumentoBrillo = new Mat(imageGray.rows(), imageGray.cols(), imageGray.type());
+
+		// Copiar imagen para las 2 resultantes
+		for (int i = 0; i < imageGray.rows(); i++) {
+			for (int j = 0; j < imageGray.cols(); j++) {
+				// Obtener el valor del pixel
+				double[] pixelValue = imageGray.get(i, j);
+
+				// Copiar el valor del pixel
+				imagenAumentoBrillo.put(i, j, pixelValue);
+				imagenDisminucionBrillo.put(i, j, pixelValue);
+			}
+		}
+
+		// Preguntar al usuario las coordenadas del pixel a modificar
+		int row = -1, col = -1;
+		while (row < 0 || row >= height || col < 0 || col >= width) {
+			String input = JOptionPane.showInputDialog("Ingrese la fila y columna del pixel a modificar (x,y)");
+			String[] coords = input.split(",");
+			row = Integer.parseInt(coords[0].trim());
+			col = Integer.parseInt(coords[1].trim());
+		}
+
+		// Obtener el valor del pixel seleccionado
+		double[] pixelValue = imageGray.get(row, col);
 
 		// Pedir valor para diferenciar de donde aclarar y desde donde disminuir
 		double valorDivisorio = pixelValue[0];
@@ -82,64 +96,60 @@ public class AclaradoOscurecimiento1Pixel {
 
 		// Definir variables para resguardar el valor maximo y minimo alcanzado por los
 		// pixeles en sus modificaciones
-		double minValue = Double.MAX_VALUE;
-		double maxValue = Double.MIN_VALUE;
-		
-		// Comenzar tratamiento de pixeles
-		double[] pixelOriginal = imageGray.get(row, col);
+		double minValue;
+		double maxValue;
 
-		// Disminucion de brillo
-		if (pixelOriginal[0] <= valorDivisorio) {
-		    double[] pixelDisminucionBrillo = new double[] { pixelValue[0] - valorDisminucionBrillo };
-		    imagenDisminucionBrillo.put(row, col, pixelDisminucionBrillo);
+		// Disminución de brillo
+		double[] pixelDisminucionBrillo = new double[] { pixelValue[0] - valorDisminucionBrillo };
+		imagenDisminucionBrillo.put(row, col, pixelDisminucionBrillo);
 
-		    // Actualiza minValue si es necesario
-		    minValue = Math.min(minValue, pixelDisminucionBrillo[0]);
-		} else {
-		    imagenDisminucionBrillo.put(row, col, pixelOriginal);
-		    // Actualiza minValue si es necesario
-		    minValue = Math.min(minValue, pixelOriginal[0]);
-		}
+		// Aumento de brillo
+		double[] pixelAumentoBrillo = new double[] { pixelValue[0] + valorAumentoBrillo };
+		imagenAumentoBrillo.put(row, col, pixelAumentoBrillo);
 
-		if (pixelOriginal[0] >= valorDivisorio) {
-		    // Aumento de brillo
-		    double[] pixelAumentoBrillo = new double[] { pixelValue[0] + valorAumentoBrillo };
-		    imagenAumentoBrillo.put(row, col, pixelAumentoBrillo);
-
-		    // Actualiza maxValue si es necesario
-		    maxValue = Math.max(maxValue, pixelAumentoBrillo[0]);
-		} else {
-		    imagenAumentoBrillo.put(row, col, pixelOriginal);
-		    // Actualiza maxValue si es necesario
-		    maxValue = Math.max(maxValue, pixelOriginal[0]);
-		}
+		maxValue = pixelAumentoBrillo[0];
+		minValue = pixelDisminucionBrillo[0];
 
 		// Reajustar los pixeles de imagenAumentoBrillo
 		int respuestaAumentar = 100;
 		int respuestaDisminuir = 100;
 		if (maxValue > 255) {
-		    respuestaAumentar = JOptionPane.showConfirmDialog(null,"¿Quieres reajustar la imagen que aumentara el brillo?", "Reajustar",JOptionPane.YES_NO_CANCEL_OPTION);
-		    if (respuestaAumentar == JOptionPane.YES_OPTION) {
-		        double[] pixelAumentoBrillo = imagenAumentoBrillo.get(row, col);
-		        pixelAumentoBrillo[0] = (pixelAumentoBrillo[0] / maxValue) * 255;
-		        imagenAumentoBrillo.put(row, col, pixelAumentoBrillo);
-		    } else if (respuestaAumentar == JOptionPane.NO_OPTION) {
-		        JOptionPane.showMessageDialog(null,"No se reajustara la imagen apesar que algunos pixeles tienen valores mayores a 255");
-		    }
+			respuestaAumentar = JOptionPane.showConfirmDialog(null,"¿Quieres reajustar la imagen que aumentara el brillo?", "Reajustar",JOptionPane.YES_NO_CANCEL_OPTION);
+			if (respuestaAumentar == JOptionPane.YES_OPTION) {
+				for(int i = 0; i<imageGray.rows(); i++) {
+					row = i;
+					for(int j = 0; j<imageGray.cols(); j++) {
+						col = j;
+						pixelAumentoBrillo = imagenAumentoBrillo.get(row, col);
+						pixelAumentoBrillo[0] = (pixelAumentoBrillo[0] / maxValue) * 255;
+						imagenAumentoBrillo.put(row, col, pixelAumentoBrillo);
+					}
+				}
+
+			} else if (respuestaAumentar == JOptionPane.NO_OPTION) {
+				JOptionPane.showMessageDialog(null,"No se reajustara la imagen apesar que algunos pixeles tienen valores mayores a 255");
+			}
 		}
-		
+
 		// Reajustar los pixeles de imagenDisminucionBrillo
 		if (minValue < 0) {
 			respuestaDisminuir = JOptionPane.showConfirmDialog(null, "¿Quieres reajustar la imagen que disminuira el brillo?","Reajustar", JOptionPane.YES_NO_CANCEL_OPTION);
 			if (respuestaDisminuir == JOptionPane.YES_OPTION) {
-				double[] pixelDisminucionBrillo = imagenDisminucionBrillo.get(row, col);
-				pixelDisminucionBrillo[0] = ((pixelDisminucionBrillo[0] - minValue) / (valorDivisorio - minValue)) * 255;
-				imagenDisminucionBrillo.put(row, col, pixelDisminucionBrillo);
+				for(int i = 0; i<imageGray.rows(); i++) {
+					row = i;
+					for(int j = 0; j<imageGray.cols(); j++) {
+						col = j;
+						pixelDisminucionBrillo = imagenDisminucionBrillo.get(row, col);
+						pixelDisminucionBrillo[0] = ((pixelDisminucionBrillo[0] - minValue) / (valorDivisorio - minValue)) * 255;
+						imagenDisminucionBrillo.put(row, col, pixelDisminucionBrillo);
+					}
+				}
+
 			} else if (respuestaDisminuir == JOptionPane.NO_OPTION) {
 				JOptionPane.showMessageDialog(null,"No se reajustara la imagen apesar que algunos pixeles tienen valores menores a 0");
 			}
 		}
-		
+
 
 		// Seleccionar la carpeta destino para guardar la imagen transformada
 		CarpetaDestino carpeta = new CarpetaDestino();
@@ -157,39 +167,39 @@ public class AclaradoOscurecimiento1Pixel {
 		} else {
 			Imgcodecs.imwrite(carpetaDestino + "./Oscurecimiento.jpg", imagenDisminucionBrillo);
 		}
-		
+
 		//Guardar matriz de imagen aumentada
 		try {
-		    FileWriter writer = new FileWriter(carpetaDestino + "/ImagenMatrizAclarada.csv");
+			FileWriter writer = new FileWriter(carpetaDestino + "/ImagenMatrizAclarada.csv");
 
-		    for (int i = 0; i < imagenAumentoBrillo.rows(); i++) {
-		        for (int j = 0; j < imagenAumentoBrillo.cols(); j++) {
-		            double[] value = imagenAumentoBrillo.get(i, j);
-		            writer.write(String.valueOf(value[0]) + ",");
-		        }
-		        writer.write("\n");
-		    }
+			for (int i = 0; i < imagenAumentoBrillo.rows(); i++) {
+				for (int j = 0; j < imagenAumentoBrillo.cols(); j++) {
+					double[] value = imagenAumentoBrillo.get(i, j);
+					writer.write(String.valueOf(value[0]) + ",");
+				}
+				writer.write("\n");
+			}
 
-		    writer.close();
+			writer.close();
 		} catch (IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
 
 		//Guardar matriz de imagen disminuida
 		try {
-		    FileWriter writer = new FileWriter(carpetaDestino + "/ImagenMatrizOscurecida.csv");
+			FileWriter writer = new FileWriter(carpetaDestino + "/ImagenMatrizOscurecida.csv");
 
-		    for (int i = 0; i < imagenDisminucionBrillo.rows(); i++) {
-		        for (int j = 0; j < imagenDisminucionBrillo.cols(); j++) {
-		            double[] value = imagenDisminucionBrillo.get(i, j);
-		            writer.write(String.valueOf(value[0]) + ",");
-		        }
-		        writer.write("\n");
-		    }
+			for (int i = 0; i < imagenDisminucionBrillo.rows(); i++) {
+				for (int j = 0; j < imagenDisminucionBrillo.cols(); j++) {
+					double[] value = imagenDisminucionBrillo.get(i, j);
+					writer.write(String.valueOf(value[0]) + ",");
+				}
+				writer.write("\n");
+			}
 
-		    writer.close();
+			writer.close();
 		} catch (IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
 
 	}
