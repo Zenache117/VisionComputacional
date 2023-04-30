708c5b5c27f0c11ac9a33590dd0e096bc90d0f81
Mauricio <mauricio.tamezelperro@gmail.com>
2023-04-30 11:26:08 -0600

    Vecindarios ahora se representan en colores

--- VisionComputacional/src/Portafolio/VisualizacionVecindarios.java
+++ VisionComputacional/src/Portafolio/VisualizacionVecindarios.java
@@ -12,37 +12,38 @@ public class VisualizacionVecindarios {
 
 	public void vecindariosColoreado(int width, int height, List<List<List<Vecinos>>> vecindarios, String rutaCarpetaDestino) {
 
-		// Cargar la biblioteca OpenCV
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		
-		List<Color> colores = new ArrayList<>();
+	    // Cargar la biblioteca OpenCV
+	    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-		for (int i = 0; i < vecindarios.size(); i++) {
-		    int r = (int) (Math.random() * 256);
-		    int g = (int) (Math.random() * 256);
-		    int b = (int) (Math.random() * 256);
-		    Color color = new Color(r, g, b);
-		    colores.add(color);
-		}
+	    List<Color> colores = new ArrayList<>();
 
-		// Crear una imagen en escala de grises con el mismo tamaño que los vecindarios
-		Mat image = new Mat(height, width, CvType.CV_8UC1);
+	    for (int i = 0; i < vecindarios.size(); i++) {
+	        int r = (int) (Math.random() * 256);
+	        int g = (int) (Math.random() * 256);
+	        int b = (int) (Math.random() * 256);
+	        Color color = new Color(r, g, b);
+	        colores.add(color);
+	    }
 
+	    // Crear una imagen RGB con el mismo tamaño que los vecindarios
+	    Mat image = new Mat(height, width, CvType.CV_8UC3);
 
-		// Asignar un color a cada vecindario
-		for (int i = 0; i < vecindarios.size(); i++) {
-		    for (int j = 0; j < vecindarios.get(i).size(); j++) {
-		        List<Vecinos> vecindario = vecindarios.get(i).get(j);
-		        int colorIndex = i; // Cálculo del índice del color
-		        Color color = colores.get(colorIndex);
-		        for (Vecinos pixel : vecindario) {
-		            image.put(pixel.getI(), pixel.getJ(), color.getRed()); // Asignar el valor del píxel correspondiente al valor del canal rojo del color
-		        }
-		    }
-		}
+	    // Asignar un color a cada vecindario
+	    for (int i = 0; i < vecindarios.size(); i++) {
+	        for (int j = 0; j < vecindarios.get(i).size(); j++) {
+	            List<Vecinos> vecindario = vecindarios.get(i).get(j);
+	            int colorIndex = i; // Cálculo del índice del color
+	            Color color = colores.get(colorIndex);
+	            for (Vecinos pixel : vecindario) {
+	                // Asignar el valor del píxel correspondiente a los valores de los canales de color del color
+	                image.put(pixel.getI(), pixel.getJ(), color.getRed(), color.getGreen(), color.getBlue());
+	            }
+	        }
+	    }
 
-		// Guardar la imagen vecindarios a colores
-		Imgcodecs.imwrite(rutaCarpetaDestino + "./VecindariosColoreados.jpg", image);
-		
+	    // Guardar la imagen vecindarios a colores
+	    Imgcodecs.imwrite(rutaCarpetaDestino + "./VecindariosColoreados.jpg", image);
+	    
 	}
+
 }
