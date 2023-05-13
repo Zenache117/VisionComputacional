6f79e1bc5f9366ba4c3c53791483d1554f6301c2
Mauricio <mauricio.tamezelperro@gmail.com>
2023-05-13 13:00:29 -0600

    Transformada de Hough test

--- VisionComputacional/src/Portafolio/DeteccionLineas.java
+++ VisionComputacional/src/Portafolio/DeteccionLineas.java
@@ -7,8 +7,12 @@ import java.util.List;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.jfree.data.category.DefaultCategoryDataset;
 import org.opencv.core.Core;
+import java.io.FileWriter;
+
+
+
+
 
 public class DeteccionLineas {
 
@@ -26,57 +30,74 @@ public class DeteccionLineas {
 
 		DeteccionLineas detec = new DeteccionLineas();
 		matBordes = detec.leerMatriz(rutaArchivo);
-
-		// --------------------------------------------------------------------------------------------------------------------
-
-		// Numero de filas y columnas en la matriz de bordes
-		int rows = matBordes.size();
-		int cols = matBordes.get(0).size();
-
-		// Rango de valores para rho y theta
-		double rhoMin = 0;
-		double rhoMax = Math.hypot(rows, cols);
-		double thetaMin = 0;
-		double thetaMax = Math.PI;
-
-		// Numero de contenedores para rho y theta
-		int rhoBins = 200;
-		int thetaBins = 200;
-
-		// Tama�o de los contenedores para rho y theta
-		double rhoStep = (rhoMax - rhoMin) / rhoBins;
-		double thetaStep = (thetaMax - thetaMin) / thetaBins;
-
-		// Crear el acumulador y llenarlo con ceros
-		int[][] accumulator = new int[rhoBins][thetaBins];
-		for (int i = 0; i < rhoBins; i++) {
-			for (int j = 0; j < thetaBins; j++) {
-				accumulator[i][j] = 0;
-			}
-		}
-
-		// Para cada pixel de borde en la matriz de bordes
-		for (int y = 0; y < rows; y++) {
-			for (int x = 0; x < cols; x++) {
-				if (matBordes.get(y).get(x) == 1) {
-					// Calcular los valores de rho para cada valor de theta
-					for (int i = 0; i < thetaBins; i++) {
-						double theta = thetaMin + i * thetaStep;
-						double rho = x * Math.cos(theta) + y * Math.sin(theta);
-						int j = (int) Math.round((rho - rhoMin) / rhoStep);
-						accumulator[j][i]++;
-					}
-				}
-			}
-		}
+		
+		int[][] accumulator = detec.houghTransform(matBordes);
 
 		// Seleccionar la carpeta destino para guardar la imagen transformada
 		CarpetaDestino carpetaDestino = new CarpetaDestino();
 		String rutaCarpetaDestino = carpetaDestino.selectCarpet();
 
+		//Guardar matriz de imagen copiada
+		try {
+		    FileWriter writer = new FileWriter(rutaCarpetaDestino + "/TransformadaHough.csv");
+
+		    for (int i = 0; i < matBordes.size(); i++) {
+		        for (int j = 0; j < matBordes.get(0).size(); j++) {
+		            int value = accumulator[i][j];
+		            writer.write(String.valueOf(value) + ",");
+		        }
+		        writer.write("\n");
+		    }
+
+		    writer.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+	
+		
+
 		// -----------------------------------------------------------------------------------------------------------------------
 
 		System.out.println("");
+	}
+
+	public int[][] houghTransform(List<List<Integer>> matBordes) {
+		int rows = matBordes.size();
+		int cols = matBordes.get(0).size();
+		int[][] votingSpace = new int[rows][cols];
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				if (matBordes.get(i).get(j) == 0) { // pixel borde
+					votingSpace[i][j]++;
+					// verificar píxeles adyacentes
+					for (int x = -1; x <= 1; x++) {
+						for (int y = -1; y <= 1; y++) {
+							if (x == 0 && y == 0)
+								continue;
+							int newX = i + x;
+							int newY = j + y;
+							if (newX >= 0 && newX < rows && newY >= 0 && newY < cols
+									&& matBordes.get(newX).get(newY) == 0) {
+								votingSpace[newX][newY]++;
+								// verificar píxeles consecutivos en la misma dirección
+								int nextX = newX + x;
+								int nextY = newY + y;
+								while (nextX >= 0 && nextX < rows && nextY >= 0 && nextY < cols
+										&& matBordes.get(nextX).get(nextY) == 0) {
+									votingSpace[nextX][nextY]++;
+									nextX += x;
+									nextY += y;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return votingSpace;
 	}
 
 	public List<List<Integer>> leerMatriz(String rutaArchivo) {
@@ -88,7 +109,7 @@ public class DeteccionLineas {
 				for (String valor : csvRecord) {
 
 					// Los valores se leen automaticamente como double por la libreria ApacheCSV y
-					// se reguarda como strinng por eso se hace una doble conversión de string a
+					// se reguarda como strinng por eso se hace una doble conversiÃ³n de string a
 					// double y de double a integer
 					fila.add(Double.valueOf(Double.parseDouble(valor)).intValue());
 				}
